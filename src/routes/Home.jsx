28ab609aee88f57bf8f4c24aa1b2226fb5852a73
Ariel Vieira <arielvieira65@gmail.com>
2023-02-10 12:14:19 -0300

    Feat: Add edit and remove a cat

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import catAdoptionFetch from '../axios/config'
 
 const Home = () => {
-    // resgatar dados de gatos em uma variavel
+    // Resgata dados de gatos e armazena em uma constante
     const [cats, setCats] = useState([])
     const getCats = async () => {
         try {
@@ -15,21 +15,48 @@ const Home = () => {
         }
     }
 
-    // para cadastrar um novo gato
-    const [name, setName] = useState([])
-    const [age, setAge] = useState([])
-    const [weight, setWeight] = useState([])
-    const [breed, setBreed] = useState([])
-    const [gender, setGender] = useState([])
-    const [color, setColor] = useState([])
+    // Cadastro de um novo gato
+    const [name, setName] = useState("")
+    const [age, setAge] = useState(0)
+    const [weight, setWeight] = useState(0)
+    const [breed, setBreed] = useState("")
+    const [gender, setGender] = useState("")
+    const [color, setColor] = useState("")
 
     const createCat = async (e) => {
-        // e.preventDefault()
-
         const cat = { name, age, weight, breed, gender, color }
         const catJson = JSON.stringify(cat)
 
-        await catAdoptionFetch.post("/cat", catJson)
+        await catAdoptionFetch.post("/cat", cat)
+    }
+
+    // Edição de um gato
+    const [id, setId] = useState(0)
+    const editCat = (e, id) => {
+        const cat = cats.find(cat => cat.id == id)
+
+        setId(cat.id)
+        setName(cat.name)
+        setAge(cat.age)
+        setWeight(cat.weight)
+        setColor(cat.color)
+        setGender(cat.gender)
+        setBreed(cat.breed)
+    }
+
+    const updateCat = async (e) => {
+        const cat = { name, age, weight, breed, gender, color }
+        const catJson = JSON.stringify(cat)
+
+        await catAdoptionFetch.put(`/cat/${id}`, catJson)
+    }
+
+    const deleteCat = async (e, id) => {
+        const cat = cats.find(cat => cat.id == id)
+
+        await catAdoptionFetch.delete(`/cat/${cat.id}`)
+
+        window.location.reload()
     }
 
     useEffect(() => {
@@ -71,8 +98,13 @@ const Home = () => {
                                         <td>{cat.gender}</td>
                                         <td>{cat.color}</td>
                                         <td>
-                                            <button className="btn btn-warning me-2">Editar</button>
-                                            <button className="btn btn-danger">Remover</button>
+                                            <button type="button" className="btn btn-warning me-2"
+                                                data-bs-toggle="modal"
+                                                data-bs-target="#editCatModal"
+                                                onClick={(e) => editCat(e, cat.id)}>
+                                                Editar
+                                            </button>
+                                            <button className="btn btn-danger" onClick={(e) => deleteCat(e, cat.id)}>Remover</button>
                                         </td>
                                     </tr>
                                 ))
@@ -82,6 +114,7 @@ const Home = () => {
                 </div>
             </div>
 
+            {/* Modal de cadastro de gato */}
             <div className="modal fade" id="addNewCatModal" tabIndex={-1}>
                 <div className="modal-dialog modal-dialog-centered">
                     <div className="modal-content">
@@ -120,6 +153,50 @@ const Home = () => {
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
                             <button form="newCatForm" className="btn btn-primary">Cadastrar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Modal de edição de gato */}
+            <div className="modal fade" id="editCatModal" tabIndex={-1}>
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Editar gatinho</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <form id="editCatForm" onSubmit={(e) => updateCat(e)}>
+                                <div className="mb-3">
+                                    <label htmlFor="nameInput" className="form-label">Nome</label>
+                                    <input value={name} onChange={(e) => setName(e.target.value)} type="text" className="form-control" id="nameInput" aria-describedby="nameHelp" />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="ageInput" className="form-label">Idade</label>
+                                    <input value={age} onChange={(e) => setAge(e.target.value)} type="number" className="form-control" id="ageInput" />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="weightInput" className="form-label">Peso</label>
+                                    <input value={weight} onChange={(e) => setWeight(e.target.value)} type="number" className="form-control" id="weightInput" />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="breedInput" className="form-label">Raça</label>
+                                    <input value={breed} onChange={(e) => setBreed(e.target.value)} type="text" className="form-control" id="breedInput" />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="genderInput" className="form-label">Gênero</label>
+                                    <input value={gender} onChange={(e) => setGender(e.target.value)} type="text" className="form-control" id="genderInput" />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="colorInput" className="form-label">Cor</label>
+                                    <input value={color} onChange={(e) => setColor(e.target.value)} type="text" className="form-control" id="colorInput" />
+                                </div>
+                            </form>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                            <button form="editCatForm" className="btn btn-primary">Atualizar</button>
                         </div>
                     </div>
                 </div>

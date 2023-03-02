eb5a47f4219e8d2a42b21f0ba450fffd45aa79fc
Ariel Vieira <arielvieira65@gmail.com>
2023-03-02 11:49:54 -0300

    Feat: Add bootstrap package

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
 
 function App() {
   return (
